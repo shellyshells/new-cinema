bb9620c03fea9194a347585d0024b7c698d0ec00
shellyshells <michel.mustafov@ynov.com>
2026-02-17 15:08:00 +0100

    feat: wire up all routes in front controller

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 session_start();
 
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > 1800)) {
+    session_destroy();
+    header('Location: index.php?action=login&timeout=1');
+    exit;
+}
+if (isset($_SESSION['user_id'])) {
+    $_SESSION['last_activity'] = time();
+}
+
 spl_autoload_register(function (string $class) {
     $paths = [
         __DIR__ . '/app/models/' . $class . '.php',
@@ -56,8 +65,68 @@ switch ($action) {
         (new AuthController())->logout();
         break;
 
+    case 'movie':
+        (new MovieController())->show();
+        break;
+
+    case 'reservations':
+        (new ReservationController())->index();
+        break;
+    case 'reserve':
+        (new ReservationController())->create();
+        break;
+    case 'store_reservation':
+        (new ReservationController())->store();
+        break;
+    case 'cancel_reservation':
+        (new ReservationController())->cancel();
+        break;
+
+    // Admin
+    case 'admin_movies':
+        (new AdminController())->movies();
+        break;
+    case 'create_movie':
+        (new AdminController())->createMovie();
+        break;
+    case 'update_movie':
+        (new AdminController())->updateMovie();
+        break;
+    case 'delete_movie':
+        (new AdminController())->deleteMovie();
+        break;
+    case 'admin_screenings':
+        (new AdminController())->screenings();
+        break;
+    case 'create_screening':
+        (new AdminController())->createScreening();
+        break;
+    case 'delete_screening':
+        (new AdminController())->deleteScreening();
+        break;
+    case 'admin_reservations':
+        (new AdminController())->reservations();
+        break;
+    case 'admin_rooms':
+        (new AdminController())->rooms();
+        break;
+    case 'create_room':
+        (new AdminController())->createRoom();
+        break;
+    case 'update_room':
+        (new AdminController())->updateRoom();
+        break;
+    case 'delete_room':
+        (new AdminController())->deleteRoom();
+        break;
+    case 'admin_users':
+        (new AdminController())->users();
+        break;
+    case 'admin_delete_user':
+        (new AdminController())->deleteUser();
+        break;
+
     default:
-        // Redirect to login for now
-        header('Location: index.php?action=login');
+        (new MovieController())->index();
         break;
 }
